00c69ddec81c737a4bd26913e270a2264df8d2d2
piet <pprotassow@posteo.de>
2025-05-13 12:20:50 +0200

    fbv, cbv, gcbv, templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, HomePageView2
 
 urlpatterns = [
-    path('', home_page_view, name='home')
+    # path('', home_page_view, name='home'),
+    path('', HomePageView2.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Option 1: function based
 def home_page_view(request):
-    return HttpResponse("Hello World!")
+    return render(request, "home.html")
+
+# Option 2: class based
+class HomePageView(View):
+    def get(self, request):
+        return render(request, "home.html")
+
+# option 3: generic class based
+class HomePageView2(TemplateView):
+    template_name = "home.html"
